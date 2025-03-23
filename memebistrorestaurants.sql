c5d704b6d4d5b45ae177a881dd2759a12728acdf
2509roblox <2509roblox@gmail.com>
2025-03-23 21:13:00 +0700

    aaas

--- memebistrorestaurants.sql
+++ memebistrorestaurants.sql
@@ -1,23 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Mar 23, 2025 at 02:11 PM
--- Server version: 8.0.30
--- PHP Version: 8.2.27
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `delicioz`
 --
 
