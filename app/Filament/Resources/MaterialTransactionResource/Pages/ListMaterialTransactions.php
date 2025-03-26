430056370cb7c205e5da1d1e25beedfbe90d38bd
2509roblox <2509roblox@gmail.com>
2025-03-26 19:02:59 +0700

    abbcc

--- app/Filament/Resources/MaterialTransactionResource/Pages/ListMaterialTransactions.php
+++ app/Filament/Resources/MaterialTransactionResource/Pages/ListMaterialTransactions.php
@@ -14,7 +14,8 @@ class ListMaterialTransactions extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label('Tạo phiếu nhập xuất nguyên liệu'),
         ];
     }
 }
